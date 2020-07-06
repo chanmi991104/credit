ec758d9ad7c79cf4d60363c50b349c87cc6bfbb3
chanmi991104 <chanmi1104@hs.ac.kr>
2020-07-06 12:13:57 +0900

    post

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    time = models.CharField(max_length=200)
+    time = models.CharField(null=True, max_length=200)
 
 class Credit(models.Model):
-    title = models.CharField(max_length=100)
-    credit = models.CharField(max_length=200)
-    grade = models.CharField(max_length=200)
-    time = models.CharField(max_length=200)
+    title = models.CharField(null=True, max_length=100)
+    credit = models.CharField(null=True, max_length=200)
+    grade = models.CharField(null=True, max_length=200)
+    time = models.CharField(null=True, max_length=200)
